b2cb8cc552a2917d605044c17426a9c3e07294e0
bar <foo@example.org>
2021-11-02 23:04:17 +0800

    ..

--- fetch.js
+++ fetch.js
@@ -1,15 +1,15 @@
 const fetch = require('node-fetch');
 
-let directfetch = async ({ url }) => {
+let directfetch = async url => {
   const response = await fetch(url);
   const body = await response.text();
 
   return body;
 }
 
-let proxyfetch = async ({ url, httpProxy }) => {
+let proxyfetch = async (url, proxyOpts) => {
   const HttpsProxyAgent = require('https-proxy-agent');
-  const proxyAgent = new HttpsProxyAgent(httpProxy);
+  const proxyAgent = new HttpsProxyAgent(proxyOpts);
   const response = await fetch(url, { agent: proxyAgent});
   const body = await response.text();
 
@@ -19,12 +19,12 @@ let proxyfetch = async ({ url, httpProxy }) => {
 
 
 (async () => {
-  let url = 'https://httpbin.org/ip';
-  let httpProxy = 'http://127.0.0.1:8118';
-  let r1 = await directfetch({url});
+  let endpoint = 'https://httpbin.org/ip';
+  let r1 = await directfetch(endpoint);
   console.log(`direct fetch: ${r1}`)
 
-  let r2 = await proxyfetch({url, httpProxy});
+  let httpProxy = 'http://127.0.0.1:8118';
+  let r2 = await proxyfetch(endpoint, httpProxy);
   console.log(`proxied fetch: ${r2}`)
 
 })();
